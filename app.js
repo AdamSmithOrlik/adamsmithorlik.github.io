7b6c2d24987e1c2477491872790b52974d9dcc5f
AdamSmithOrlik <adam.smith2214@gmail.com>
2023-05-15 15:39:10 -0400

    Added menu and extra pages.

--- app.js
+++ app.js
@@ -16,3 +16,40 @@ switcher.addEventListener('click', function() {
     console.log('current class name: ' + className);
 
 });
+
+// Switching page animation
+
+// Get all the links that trigger page transitions
+const pageLinks = document.querySelectorAll('a[data-page-link]');
+
+// Add click event listeners to the links
+pageLinks.forEach(link => {
+  link.addEventListener('click', handlePageTransition);
+});
+
+// Handle page transition
+function handlePageTransition(e) {
+  e.preventDefault();
+
+  // Get the target page
+  const targetPage = e.target.getAttribute('href');
+
+  // Add the 'page-leave' class to the current page
+  const currentPage = document.querySelector('.page.active');
+  currentPage.classList.add('page-leave');
+
+  // Remove the 'active' class from the current page after the animation completes
+  setTimeout(() => {
+    currentPage.classList.remove('active');
+  }, 500);
+
+  // Add the 'page-enter' class to the target page
+  const nextPage = document.querySelector(targetPage);
+  nextPage.classList.add('page-enter');
+
+  // Add the 'active' class to the target page after a short delay
+  setTimeout(() => {
+    nextPage.classList.add('active');
+    nextPage.classList.remove('page-enter');
+  }, 10);
+}
