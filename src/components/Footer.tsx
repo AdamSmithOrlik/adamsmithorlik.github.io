34d08ec0c3b19fec3bc70db90a5de396b3899f1d
AdamSmithOrlik <adam.smith2214@gmail.com>
2024-04-30 10:07:38 -0400

    Rocket to bottom right and synthwave for dark mode.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,26 +1,3 @@
-// import React from 'react';
-// import './Footer.css'; // We will create this CSS file next
-
-// const Footer: React.FC = () => {
-//     return (
-//         <footer className="footer">
-//             <div className="footer-contact">
-//                 <h3>Contact Me</h3>
-//                 <ul>
-//                     <li>Email: example@email.com</li>
-//                     <li>LinkedIn: linkedin.com/in/yourprofile</li>
-//                     {/* Add more contact methods as needed */}
-//                 </ul>
-//             </div>
-//             <div className="footer-copy">
-//                 © {new Date().getFullYear()} Adam Smith Orlik. All rights reserved.
-//             </div>
-//         </footer>
-//     );
-// }
-
-// export default Footer;
-
 import React from 'react';
 import './Footer.css'; // Ensure to update the CSS file with the provided styles
 
@@ -31,6 +8,7 @@ const Footer: React.FC = () => {
                 <h3>Contact</h3>
                 <ul>
                     <li>Email: <a href="mailto:asorlik@yorku.ca">asorlik@yorku.ca</a> </li>
+                    <li>Personal Email: <a href="mailto:adam.smith2214@gmail.com">adam.smith2214@gmail.com</a> </li>
                     <li>Number: 1+(613)-898-4555</li>
                 </ul>
             </div>
@@ -41,6 +19,7 @@ const Footer: React.FC = () => {
                     <li>GitHub: <a href="https://github.com/AdamSmithOrlik" target="_blank" rel="noopener noreferrer">github.com/AdamSmithOrlik</a></li>
                     <li>Twitter: <a href="https://twitter.com/AddySmith4" target="_blank" rel="noopener noreferrer">@AddySmith4</a></li>
                     <li>LinkedIn: <a href="https://www.linkedin.com/in/adam-smith-orlik-4a7b99211/" target="_blank" rel="noopener noreferrer">linkedin.com/in/adam-smith-orlik</a></li>
+                    <li>Instagram: <a href="https://www.instagram.com/addy_2214/" target="_blank" rel="noopener noreferrer">@addy_2214</a></li>
                 </ul>
             </div>
             <div className="footer-column">
